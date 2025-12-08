53f1f82ac3a354046733accac4203eee52696713
cameron-leckie <114110397+cameron-leckie@users.noreply.github.com>
2025-12-08 16:49:27 +0000

    Added test functions file to start testing

---
+++ Exercises/Lab3and4/TestFiniteFunctions.cxx
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "FiniteFunctions.h"
+
+using namespace std;
+
+int main(){
+    FiniteFunction func();
+
+    cout << "Range min = " << func.rangeMin() << "\n";
+    cout << "Range max = " << func.rangeMax() << "\n";
+
+}
